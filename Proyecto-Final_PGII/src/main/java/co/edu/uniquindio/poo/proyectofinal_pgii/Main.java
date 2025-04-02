0b99d5de9e6ebca1b66e733df07fd6eaed8e9774
glCristian <cristianc.bonillap@uqvirtual.edu.co>
2025-04-02 11:23:15 -0500

    Creación del proyecto

---
+++ Proyecto-Final_PGII/src/main/java/co/edu/uniquindio/poo/proyectofinal_pgii/Main.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.poo.proyectofinal_pgii;
+
+
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("¡JavaFX funcionando en JDK 23!");
+        Scene scene = new Scene(label, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("Prueba JavaFX");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
