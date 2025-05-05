816f731ce5b3a58e0180718999a03b99d876e9bc
glCristian <cristianc.bonillap@uqvirtual.edu.co>
2025-05-04 22:48:55 -0500

    nada importantee

--- Proyecto_Final_PGII/src/main/java/co/edu/uniquindio/poo/proyecto_final_pgii/app/App.java
+++ Proyecto_Final_PGII/src/main/java/co/edu/uniquindio/poo/proyecto_final_pgii/app/App.java
@@ -1,4 +1,4 @@
-package co.edu.uniquindio.poo.proyecto_final_pgii;
+package co.edu.uniquindio.poo.proyecto_final_pgii.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/co/edu/uniquindio/poo/proyecto_final_pgii/menuInicio.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Inicio");
         stage.setScene(scene);
         stage.show();
     }
--- Proyecto_Final_PGII/src/main/java/module-info.java
+++ Proyecto_Final_PGII/src/main/java/module-info.java
@@ -4,5 +4,13 @@ module co.edu.uniquindio.poo.proyecto_final_pgii {
     requires java.xml;
 
     opens co.edu.uniquindio.poo.proyecto_final_pgii.viewController to javafx.fxml;
-    exports co.edu.uniquindio.poo.proyecto_final_pgii;
+    exports co.edu.uniquindio.poo.proyecto_final_pgii.viewController;
+
+    opens co.edu.uniquindio.poo.proyecto_final_pgii.app to javafx.fxml;
+    exports co.edu.uniquindio.poo.proyecto_final_pgii.app;
+
+    opens co.edu.uniquindio.poo.proyecto_final_pgii.model to javafx.fxml;
+    exports co.edu.uniquindio.poo.proyecto_final_pgii.model;
+
+
 }
